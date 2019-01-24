fe75174ff6ab3c1a82d75e959a9ded377d4dd533
Dan Barber <d.j.barber88@gmail.com>
2019-01-24 14:48:25 +0000

    changed enviroment variable

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -3,7 +3,7 @@ import styled, { keyframes } from 'styled-components'
 import fiveStars from '../images/five-stars.svg'
 import reviewPlaceholder from '../images/review-placeholder.svg'
 import caret from '../images/caret-right.svg'
-import Button from "./button"
+import Button from './button'
 
 // BUG: Reviews go into a single line on IE, probably a flex issue.
 
@@ -71,7 +71,7 @@ class Reviews extends Component {
     this.state = {
       reviews: [],
       loading: true,
-      error: false
+      error: false,
     }
   }
 
@@ -87,7 +87,7 @@ class Reviews extends Component {
 
     service.getDetails(request, (place, status) => {
       // Latest 5 star reviews.
-      if(status === "OK") {
+      if (status === 'OK') {
         const reviews = place.reviews
           .filter(review => review.rating === 5 && review.text.length > 0)
           .sort((a, b) => b.time - a.time)
@@ -98,7 +98,7 @@ class Reviews extends Component {
       } else {
         this.setState({
           loading: false,
-          error: true
+          error: true,
         })
       }
     })
@@ -107,9 +107,9 @@ class Reviews extends Component {
   tryAgain = () => {
     this.setState({
       loading: true,
-      error: false
+      error: false,
     })
-    this.getReviews();
+    this.getReviews()
   }
 
   componentDidMount() {
@@ -118,7 +118,7 @@ class Reviews extends Component {
       const script = document.createElement('script')
       script.type = 'text/javascript'
       script.src = `https://maps.googleapis.com/maps/api/js?key=${
-        process.env.GOOGLE_API
+        process.env.GATSBY_GOOGLE_API_KEY
       }&libraries=places`
       const headScript = document.getElementsByTagName('script')[0]
       headScript.parentNode.insertBefore(script, headScript)
@@ -141,38 +141,40 @@ class Reviews extends Component {
     ]
 
     const reviews = this.state.reviews.map((review, i) => (
-        <Review key={i}>
-          <a
-            target="_blank"
-            href="https://www.google.com/search?rlz=1C1CHBF_en-GBGB784GB784&ei=YGJEXPWUIv6d1fAPps2YuA4&q=Mulberry+Fitted+Kitchens+Ltd&oq=Mulberry+Fitted+Kitchens+Ltd&gs_l=psy-ab.3..35i39l2j0i67.32730.37250..37659...0.0..0.243.3234.9j18j1......0....1..gws-wiz.......0i71j0i131i67j0j0i20i263j0i22i10i30j0i22i30.GCcKXiiCcRw#lrd=0x4878bfc0320d45eb:0xc24647140636ef53,1,,,"
-          >
-            <Name>{review.author_name}</Name>
-            <img src={fiveStars} alt="Star Rating" />
-            <p>
-              {/* Truncate review text to a max of 50 words. */}
-              {review.text.length > 350
-                ? review.text
-                    .split(' ')
-                    .slice(0, 50)
-                    .join(' ')
-                    .concat('...')
-                : review.text}
-              {review.text.length > 350 ? (
-                <span>
-                  <SmallCaret src={caret} />
-                </span>
-              ) : null}
-            </p>
-          </a>
-        </Review>
+      <Review key={i}>
+        <a
+          target="_blank"
+          href="https://www.google.com/search?rlz=1C1CHBF_en-GBGB784GB784&ei=YGJEXPWUIv6d1fAPps2YuA4&q=Mulberry+Fitted+Kitchens+Ltd&oq=Mulberry+Fitted+Kitchens+Ltd&gs_l=psy-ab.3..35i39l2j0i67.32730.37250..37659...0.0..0.243.3234.9j18j1......0....1..gws-wiz.......0i71j0i131i67j0j0i20i263j0i22i10i30j0i22i30.GCcKXiiCcRw#lrd=0x4878bfc0320d45eb:0xc24647140636ef53,1,,,"
+        >
+          <Name>{review.author_name}</Name>
+          <img src={fiveStars} alt="Star Rating" />
+          <p>
+            {/* Truncate review text to a max of 50 words. */}
+            {review.text.length > 350
+              ? review.text
+                  .split(' ')
+                  .slice(0, 50)
+                  .join(' ')
+                  .concat('...')
+              : review.text}
+            {review.text.length > 350 ? (
+              <span>
+                <SmallCaret src={caret} />
+              </span>
+            ) : null}
+          </p>
+        </a>
+      </Review>
     ))
 
     if (this.state.error) {
       return (
         <ErrorContainer>
           <h2>Failed to fetch reviews :(</h2>
-          <Button primary onClick={ this.tryAgain.bind(this) }>Try Again</Button>
-          <div id="attribution-container" style={{ display: "none"}} />
+          <Button primary onClick={this.tryAgain.bind(this)}>
+            Try Again
+          </Button>
+          <div id="attribution-container" style={{ display: 'none' }} />
         </ErrorContainer>
       )
     }
@@ -180,7 +182,7 @@ class Reviews extends Component {
     return (
       <Container>
         {this.state.loading ? placeHolders : reviews}
-        <div id="attribution-container" style={{ display: "none"}}/>
+        <div id="attribution-container" style={{ display: 'none' }} />
       </Container>
     )
   }
