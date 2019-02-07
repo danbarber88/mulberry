241b304d754e362737f95bb109f44b5d5fda5f9a
Dan Barber <d.j.barber88@gmail.com>
2019-02-07 12:25:56 +0000

    contentSection responsive

--- src/components/contentSection.js
+++ src/components/contentSection.js
@@ -1,44 +1,76 @@
 import 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { device } from '../utils/device'
 
 // FADE!?
+export const MainHeader = styled.h1`
+  font-size: 2rem;
+  border: none;
+  color: #000928;
+  font-weight: 400;
+  margin-top: 60px;
+  margin-bottom: 1.21875rem;
+`
+
+export const SecondaryHeader = styled.h2`
+  font-size: 2rem;
+  border: none;
+  color: #000928;
+  font-weight: 400;
+  margin-top: 60px;
+  margin-bottom: 1.21875rem;
+`
 
 // Ideally, ContentSections children will be two side by side ContentContainers - can contain whatever
 export const ContentSection = styled.section`
   display: flex;
-  ${props => `flex-direction: ${props.column ? 'column' : 'row'}`};
+  flex-direction: ${props => (props.column ? 'column' : 'row')};
   justify-content: space-between;
-  width: 65%;
+  flex-wrap: wrap;
+  width: 70%;
+  padding-bottom: 50px;
 
-  h1,
-  h2 {
-    font-size: 2rem;
-    border: none;
-    color: #000928;
-    font-weight: 400;
-    margin-top: 60px;
-    margin-bottom: 1.21875rem;
+  @media ${device.laptopL} {
+    width: 80%;
+  }
+
+  @media ${device.laptop} {
+    width: 70%;
+  }
+
+  @media ${device.laptop} {
+    width: 80%;
   }
 `
 
 export const ContentContainer = styled.div`
   max-width: 50%;
 
-  h3 {
-    font-weight: 400;
-    line-height: 30px;
+  width: ${props => props.width};
+  color: ${props => (props.darkBg ? '#fff' : 'inherit')};
+
+  @media ${device.laptopL} {
+    max-width: 45%;
   }
 
-  ${props => `
-    width: ${props.width};
-    color: ${props.darkBg ? '#fff' : 'inherit'}
-    margin-bottom: ${props.marginBottom ? '50px' : '0'}
-  `};
+  @media ${device.laptop} {
+    max-width: 80%;
+  }
+
+  @media ${device.tablet} {
+    max-width: 100%;
+  }
 `
+
+ContentSection.propTypes = {
+  column: PropTypes.bool,
+  wrap: PropTypes.bool,
+}
 
 ContentContainer.propTypes = {
   width: PropTypes.string, // Percentage
   darkBg: PropTypes.bool,
-  marginBottom: PropTypes.bool
+  marginTop: PropTypes.bool,
+  marginBottom: PropTypes.bool,
 }
