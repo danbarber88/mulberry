cb5486a7a38e5a4e69bf447d12c76468f44df833
Dan Barber <d.j.barber88@gmail.com>
2019-02-07 12:43:36 +0000

    latest news items responsive

--- src/components/latestNewsItem.js
+++ src/components/latestNewsItem.js
@@ -1,25 +1,51 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { device } from '../utils/device'
 
 const Header = styled.p`
   margin: 0;
   strong {
     color: #000928;
   }
+
+  @media ${device.tablet} {
+    line-height: 18px;
+  }
 `
 
 const Description = styled.p`
   margin: 0;
   padding: 0 0 10px 20px;
+
+  @media ${device.tablet} {
+    padding: 5px 0 20px 0;
+    line-height: 18px;
+  }
+`
+
+const PostedDate = styled.span`
+  @media ${device.tablet} {
+    display: block;
+    line-height: 16px;
+    font-size: 12px;
+    color: #656565;
+
+    .on {
+      display: none;
+    }
+  }
 `
 
 const LatestNewsItem = ({ heading, date, description }) => (
   <>
     <Header>
-      <strong>{heading}</strong> on {date}
+      <strong>{heading}</strong>{' '}
+      <PostedDate>
+        <span className="on">on</span> {date}
+      </PostedDate>
     </Header>
-    <Description>- {description}</Description>
+    <Description>{description}</Description>
   </>
 )
 
