c477f380c968e3038e249f768cf55952cb96a182
Dan Barber <d.j.barber@gmail.com>
2019-02-26 11:24:11 +0000

    unneeded imports

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import styled from 'styled-components'
-import { device } from '../utils/device'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
