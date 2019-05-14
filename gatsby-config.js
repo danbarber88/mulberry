466435fd21bc423e51f36a131d2f9756c723ffd2
Dan Barber <d.j.barber@gmail.com>
2019-05-14 10:15:54 +0100

    add netlify plugin and test _redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     'gatsby-transformer-remark',
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
